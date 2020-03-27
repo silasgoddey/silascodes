fbbb9f2a71a22ee6ccf397487322c999a526db32
Silas Odinma <62030888+silasgoddey@users.noreply.github.com>
2020-03-27 16:13:16 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,9 @@
+var name = "Silas Odinma";
+var courses = ["HTML", "CSS", "and JAVASCRIPT"]
+const data = [];
+for (var i =1; i < 200; i += 2) {
+    data.push(i)
+}
+console.log ("My name is " + name);
+console.log ("I offer " + courses);
+console.log (data);
